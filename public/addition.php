ab2264a615ea31c0637739bdb09fece602c30b66
Erica Lansangan <erica.lansangan10@gmail.com>
2019-06-30 17:02:06 +0800

    Integers with string

--- public/addition.php
+++
@@ -1,1 +0,0 @@
-<?php echo 'addition file'; ?>
---
+++ public/compute.php
@@ -0,0 +1,9 @@
+<?php
+  // computation
+  $num1 = 5;
+  $num2 = 10;
+  $sum = $num1 + $num2;
+
+  // display output with string
+  echo "The sum of num1 and num2 is " . $sum;
+?>
